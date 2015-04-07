25f15a69abf9ea875f4825f7d4f1ea711d588ae2
fedyanyet <fyodor.wolf@gmail.com>
2015-04-07 08:59:59 -0400

    consolidation

--- transmit.py
+++ transmit.py
@@ -1,12 +1,9 @@
 #!/Users/fwolf/Code/transmission/venv/bin/python
 
-import subprocess
-
 import transmissionrpc
 from transmissionrpc.error import TransmissionError
 
-
-def main():
+def shutdown():
     all_done = True
     try:
         tc = transmissionrpc.Client('localhost', port=9091)
@@ -15,10 +12,18 @@ def main():
                 all_done = False
                 break
         if all_done:
-            subprocess.call(['osascript', '-e',
-                             'tell application "Finder" to shut down'])
+            subprocess.call(['osascript', '-e', 'tell application "Finder" to shut down'])
+    except TransmissionError:
+        pass
+
+def pause():
+    try:
+        tc = transmissionrpc.Client('localhost', port=9091)
+        for torrent in tc.get_torrents():
+            if torrent.status != "downloading":
+                torrent.stop()
     except TransmissionError:
         pass
 
 if __name__ == "__main__":
-    main()
+    pause()
